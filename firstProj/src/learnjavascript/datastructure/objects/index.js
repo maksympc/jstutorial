2847d3d6be213dc4e0b2a7931bbc5c6f4d2fa9bd
Maksym Pinchuk <maksympc@gmail.com>
2018-01-10 12:01:28 +0200

    add examples with 'this' keyword

--- firstProj/src/learnjavascript/datastructure/objects/index.js
+++ firstProj/src/learnjavascript/datastructure/objects/index.js
@@ -56,3 +56,60 @@ function multiplyNumeric(obj) {
 multiplyNumeric(menu);
 alert("menu width=" + menu.width + " height=" + menu.height + " title=" + menu.title);
 
+//------------------------------------------
+// link:https://learn.javascript.ru/object-methods
+//------------------------------------------
+
+/**
+ * Создайте объект calculator с тремя методами:
+
+ read() запрашивает prompt два значения и сохраняет их как свойства объекта
+ sum() возвращает сумму этих двух значений
+ mul() возвращает произведение этих двух значений
+ */
+
+var calculator = {
+    read: function () {
+        this.a = +prompt("Введите первое значение:", '');
+        this.b = +prompt("Введите второе значение:", '');
+    },
+    sum: function () {
+        return this.a + this.b;
+    },
+    mul: function () {
+        return this.a * this.b;
+    }
+};
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
+
+/**
+ Есть объект «лестница» ladder:
+ Сейчас, если нужно последовательно вызвать несколько методов объекта, это можно сделать так:
+ ladder.up();
+ ladder.up();
+ ladder.down();
+ ladder.showStep(); // 1
+ Модифицируйте код методов объекта, чтобы вызовы можно было делать цепочкой, вот так:
+ ladder.up().up().down().up().down().showStep(); // 1
+ * */
+
+var ladder = {
+    step: 0,
+    up: function () { // вверх по лестнице
+        this.step++;
+        return this;
+    },
+    down: function () { // вниз по лестнице
+        this.step--;
+        return this;
+    },
+    showStep: function () { // вывести текущую ступеньку
+        alert(this.step);
+        return this;
+    }
+};
+
+ladder.up().up().down().up().down().showStep(); // 1
