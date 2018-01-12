85701669aa27f44fbdab4638841919ab86a7952a
Maksym Pinchuk <maksympc@gmail.com>
2018-01-12 12:20:44 +0200

    add examples with call and apply

--- firstProj/src/learnjavascript/basics/functions/index.js
+++ firstProj/src/learnjavascript/basics/functions/index.js
@@ -263,3 +263,50 @@ function sum1(a) {
 }
 
 alert(sum1(2)(3));
+
+/*--------------------------------------
+link:https://learn.javascript.ru/call-apply
+--------------------------------------*/
+/**
+ *
+ Создайте функцию sumArgs(), которая будет суммировать все свои аргументы:
+ function sumArgs() {
+ }
+ alert( sumArgs(1, 2, 3) ); // 6, аргументы переданы через запятую, без массива
+ Для решения примените метод reduce к arguments, используя call, apply или одалживание метода.
+ P.S. Функция sum вам не понадобится, она приведена в качестве примера использования reduce для похожей задачи.
+ */
+
+function sumArgs(arr) {
+    return [].reduce.call(arguments, function (acc, item) {
+        return acc + item;
+    })
+}
+
+alert(sumArgs(1, 2, 3)); // 6, аргументы переданы через запятую, без массива
+
+/**
+ Напишите функцию applyAll(func, arg1, arg2...), которая получает функцию func и произвольное количество аргументов.
+ Она должна вызвать func(arg1, arg2...), то есть передать в func все аргументы, начиная со второго, и возвратить результат.
+ * */
+function sum() { // суммирует аргументы: sum(1,2,3) = 6
+    return [].reduce.call(arguments, function (a, b) {
+        return a + b;
+    });
+}
+
+function mul() { // перемножает аргументы: mul(2,3,4) = 24
+    return [].reduce.call(arguments, function (a, b) {
+        return a * b;
+    });
+}
+
+function applyAll(func) {
+    var args = [].splice.call(arguments, 1);
+    return func.apply(null, args);
+}
+
+alert( applyAll(sum, 1, 2, 3) ); // 6
+alert( applyAll(mul, 2, 3, 4) ); // 24
+alert( applyAll(Math.max, 2, -2, 3) ); // 3
+alert( applyAll(Math.min, 2, -2, 3) ); // -2
